4551e319f398f900aaf1fa44a84ae890d82643c6
carlos738 <carlosasl135@hotmail.com>
2022-02-07 19:06:50 -0300

    Rest-api

---
+++ PersonApi/src/main/java/one/digitalinnovation/PersonApi/entity/Phone.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.PersonApi.entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinnovation.PersonApi.enums.PhoneType;
+public class Phone {
+}
