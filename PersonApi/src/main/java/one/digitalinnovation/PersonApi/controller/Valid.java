fda4b846994f418abd616cdb388c0fa1828acfaf
carlos738 <carlosasl135@hotmail.com>
2022-03-16 18:50:46 -0300

    PersonApi

---
+++ PersonApi/src/main/java/one/digitalinnovation/PersonApi/controller/Valid.java
@@ -0,0 +1,2 @@
+package one.digitalinnovation.personapi.controller;public @interface Valid {
+}
--- PersonApi/src/main/java/one/digitalinnovation/PersonApi/entity/Person.java
+++ PersonApi/src/main/java/one/digitalinnovation/PersonApi/entity/Person.java
@@ -1,4 +1,4 @@
-package one.digitalinnovation.PersonApi.entity;
+package one.digitalinnovation.personapi.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
